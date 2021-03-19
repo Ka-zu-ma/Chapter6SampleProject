11b1c9858cc3c21f73cc4891d77761d66db80ee3
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-19 22:06:02 +0900

    足を地面につけた状態、オブジェクト通り抜けない

--- Assets/Scripts/HeadLookWalk.cs
+++ Assets/Scripts/HeadLookWalk.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class HeadLookWalk : MonoBehaviour {
 	public float velocity = 0.7f;
+	private CharacterController controller;
 
 	// Start is called before the first frame update
 	void Start() {
-
+		controller = GetComponent<CharacterController>();
 	}
 
 	// Update is called once per frame
 	void Update() {
-		Vector3 moveDirection = Camera.main.transform.forward;
-		moveDirection *= velocity * Time.deltaTime;
-		transform.position += moveDirection;
+		//Vector3 moveDirection = Camera.main.transform.forward;
+		//moveDirection *= velocity * Time.deltaTime;
+		//moveDirection.y = 0.0f;
+		////transform.position += moveDirection;
+		//controller.Move(moveDirection);
+		controller.SimpleMove(Camera.main.transform.forward * velocity);
 	}
 }
