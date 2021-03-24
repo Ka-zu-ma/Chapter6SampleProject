e075b45562e57daeeb29aa0215255011e804b894
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-24 18:57:11 +0900

    トランポリンとレンガ

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour {
+	public float bounceForce = 1000.0f;
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Rigidbody rb = other.GetComponent<Rigidbody>();
+		if (rb != null) {
+			rb.AddForce(Vector3.up * bounceForce);
+		}
+	}
+}
