8ffb5475ed8237d1e2047c68515f741deddb2525
Kazuma_M <km.km.kzkz00@gmail.com>
2021-03-26 02:22:25 +0900

    エレベーター追加

---
+++ Assets/Scripts/Elevate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevate : MonoBehaviour {
+	public float minHeight = 1.2f;
+	public float maxHeight = 8.0f;
+	public float velocity = 1;
+
+	// Start is called before the first frame update
+	void Start() {
+		float y = transform.position.y;
+		y += velocity * Time.deltaTime;
+		if (y > maxHeight) {
+			y = maxHeight;
+			velocity = -velocity;
+		}
+
+		if (y < minHeight) {
+			y = minHeight;
+			velocity = -velocity;
+		}
+
+		transform.position = new Vector3(transform.position.x, y, transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
---
+++ Assets/Scripts/JumpGesture.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpGesture : MonoBehaviour {
+	public bool isJump = false;
+	public float jumpForce = 1000.0f;
+
+	private float jumpRate = 1.0f;
+	private float previousHeight;
+	private HeadLookWalkBounce walkBounce;
+
+	// Start is called before the first frame update
+	void Start() {
+		previousHeight = Camera.main.transform.position.y;
+		walkBounce = GetComponent<HeadLookWalkBounce>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (DetectJump()) {
+			walkBounce.bounceForce = jumpForce;
+		}
+	}
+
+	private bool DetectJump() {
+		float height = Camera.main.transform.localPosition.y;
+		float deltaHeight = height - previousHeight;
+		float rate = deltaHeight / Time.deltaTime;
+		previousHeight = height;
+		return (rate >= jumpRate);
+	}
+}
